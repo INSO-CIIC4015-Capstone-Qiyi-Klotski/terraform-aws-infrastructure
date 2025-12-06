6e7fc67609648ed99ca163e1889e09e034f71916
Janiel777 <janjannunez777@gmail.com>
2025-12-06 00:21:40 -0400

    It was switched to db.t4g.micro to perform load tests and compare it with db.t4g.small.

--- rds.tf
+++ rds.tf
@@ -49,13 +49,13 @@ resource "aws_db_instance" "postgres" {
   engine     = "postgres"
   # engine_version omitted to use AWS-recommended default
 
-  instance_class    = "db.t4g.small" # Cost-efficient instance type
+  instance_class    = "db.t4g.micro" # Cost-efficient instance type
   allocated_storage = 20
   storage_type      = "gp3"
 
   db_name  = var.db_name
   username = var.db_username
-  password = var.db_password
+  password = var.db_passworddb.t4g.micro
 
   db_subnet_group_name   = aws_db_subnet_group.pg_subnets.name
   vpc_security_group_ids = [aws_security_group.db_sg.id]
